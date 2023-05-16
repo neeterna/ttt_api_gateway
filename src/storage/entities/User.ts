f94642e075bc6e156b1f4c59252620724573a051
Vladyslav Kotliar <133151540+neeterna@users.noreply.github.com>
2023-05-16 17:00:38 +0300

    add password hashing

--- src/storage/entities/User.ts
+++ src/storage/entities/User.ts
@@ -1,6 +1,16 @@
-import { Entity, Property, Unique } from '@mikro-orm/core';
+import { Entity, Enum, Property, Unique } from '@mikro-orm/core';
+
+import { hashSync, compareSync } from 'bcrypt';
 
 import { BaseEntity } from './BaseEntity';
+
+// this is not the best solution, I know
+const pepper = "s0/QwERtyP4$$w0rD";
+
+export enum Role {
+  USER = 'user',
+  ADMIN = 'admin',
+}
 
 @Entity()
 export class User extends BaseEntity {
@@ -12,10 +22,20 @@ export class User extends BaseEntity {
   @Property()
   password_hash: string;
 
-  constructor(username: string, password_hash: string) {
+  @Enum(() => Role)
+  role!: Role;
+
+  constructor(username: string, password: string, role?: Role) {
     super();
+
     this.username = username;
-    this.password_hash = password_hash;
+    this.password_hash = hashSync(password + pepper, 10);
+    this.role = typeof (role) !== 'undefined' ? role : Role.USER;
+
+  }
+
+  isValidPassword(password: string) {
+    return compareSync(password + pepper, this.password_hash);
   }
 
 }
